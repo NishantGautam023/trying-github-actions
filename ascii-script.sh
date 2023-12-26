78caf2a49e6c2a470b65f0f077a587286feb5dda
Nishant Gautam <47427428+NishantGautam023@users.noreply.github.com>
2023-12-25 19:21:14 -0500

    Aded script file

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,13 +1,29 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-function cowsay_cli() {
+cowsay_cli() {
   echo "The program has started running"
-  sudo apt-install cowsay -y
-  cowsay -f dragon "Run for cover, I am a dragon.. RWAR" >> dragon.txt
-  grep -i "dragon" dragon.txt
+
+  # Install cowsay if it's not already installed
+  if ! command -v cowsay >/dev/null; then
+    sudo apt-get install cowsay -y
+  fi
+
+  # Create or overwrite dragon.txt with the output of cowsay
+  cowsay -f dragon "Run for cover, I am a dragon.. RWAR" > dragon.txt
+
+  # Check if the word "dragon" is in the file
+  if grep -qi "dragon" dragon.txt; then
+    echo "The word 'dragon' was found in dragon.txt"
+  else
+    echo "The word 'dragon' was not found in dragon.txt"
+  fi
+
+  # Display the contents of dragon.txt
   cat dragon.txt
+
+  # List files in the current directory
   ls -ltra
 }
 
-# shellcheck disable=SC1073
-cowsay_cli()
+# Call the function
+cowsay_cli
